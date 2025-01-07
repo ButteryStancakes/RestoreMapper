220c2722940a749ac80a837da3cde1f34859b1b1
ButteryStancakes <jlj3322@gmail.com>
2025-01-07 05:18:55 -0500

    increment version number

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ namespace RestoreMapper
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.restoremapper", PLUGIN_NAME = "Restore Mapper", PLUGIN_VERSION = "1.0.0";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.restoremapper", PLUGIN_NAME = "Restore Mapper", PLUGIN_VERSION = "1.1.0";
         internal static new ManualLogSource Logger;
 
         void Awake()
