c26dc48f01a5ff4e76153f3bc971d125eb411d73
ButteryStancakes <jlj3322@gmail.com>
2025-01-07 05:18:38 -0500

    completely reprogrammed functionality to fix outdated (thus buggy) vanilla behavior

--- Plugin.cs
+++ Plugin.cs
@@ -1,10 +1,13 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using GameNetcodeStuff;
 using HarmonyLib;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace RestoreMapper
@@ -80,6 +83,117 @@ namespace RestoreMapper
                 Item cardboardBox = itemsList?.FirstOrDefault(item => item.itemIcon?.name == "caticontest");
                 if (cardboardBox != null)
                     mapperTool.itemIcon = cardboardBox.itemIcon;
+            }
+        }
+
+        [HarmonyPatch(typeof(MapDevice), nameof(MapDevice.Start))]
+        [HarmonyPostfix]
+        static void MapDevicePostStart(MapDevice __instance)
+        {
+            // copy the camera object
+            RenderTexture orig = __instance.mapCamera.targetTexture;
+            __instance.mapCamera = Object.Instantiate(__instance.mapCamera.gameObject, __instance.mapCamera.transform.parent).GetComponent<Camera>();
+            __instance.mapCamera.tag = "Untagged";
+            __instance.mapCamera.targetTexture = new(orig.width, orig.height, orig.depth, orig.format);
+            Plugin.Logger.LogDebug($"Mapper #{__instance.GetInstanceID()} cam&tex cloned");
+
+            // get refs
+            __instance.mapAnimatorTransition = __instance.mapCamera.GetComponentInChildren<Animator>();
+            __instance.mapLight = __instance.mapCamera.GetComponentInChildren<Light>();
+
+            // performance
+            __instance.mapCamera.gameObject.SetActive(false);
+            __instance.mapCamera.enabled = true;
+
+            // set up the light
+            __instance.mapLight.enabled = false;
+            Plugin.Logger.LogDebug($"Mapper #{__instance.GetInstanceID()} light setup");
+
+            // assign texture to screen
+            MeshRenderer rend = __instance.GetComponentInChildren<MeshRenderer>();
+            Material[] mats = rend.materials;
+            mats.FirstOrDefault(mat => mat.name.StartsWith("MapScreen")).mainTexture = __instance.mapCamera.targetTexture;
+            rend.materials = mats;
+            Plugin.Logger.LogDebug($"Mapper #{__instance.GetInstanceID()} screen retex'd");
+        }
+
+        [HarmonyPatch(typeof(MapDevice), nameof(MapDevice.ItemActivate))]
+        [HarmonyPrefix]
+        static bool MapDeviceItemActivate(MapDevice __instance, /*bool used, bool buttonDown,*/ Coroutine ___pingMapCoroutine)
+        {
+            if (__instance.playerHeldBy == null || __instance.mapCamera.CompareTag("MapCamera"))
+                return true;
+
+            if (___pingMapCoroutine != null)
+                __instance.StopCoroutine(___pingMapCoroutine);
+
+            __instance.StartCoroutine(pingMapSystem(__instance, __instance.playerHeldBy));
+
+            // base.ItemActivate(used, buttonDown);
+            return false;
+        }
+
+        [HarmonyPatch(typeof(NetworkBehaviour), nameof(NetworkBehaviour.OnDestroy))]
+        [HarmonyPostfix]
+        static void NetworkBehaviourPostOnDestroy(NetworkBehaviour __instance)
+        {
+            if (__instance is MapDevice mapDevice && mapDevice.mapCamera != null && !mapDevice.mapCamera.CompareTag("MapCamera"))
+            {
+                // clean up created objects
+                if (mapDevice.mapCamera.targetTexture.IsCreated())
+                    mapDevice.mapCamera.targetTexture.Release();
+                Object.Destroy(mapDevice.mapCamera.targetTexture);
+                Object.Destroy(mapDevice.mapCamera.gameObject);
+                Plugin.Logger.LogDebug($"Mapper #{__instance.GetInstanceID()} cleaned up");
+            }
+        }
+
+        [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.UseItemBatteries))]
+        [HarmonyPrefix]
+        static bool GrabbableObjectPreUseItemBatteries(GrabbableObject __instance)
+        {
+            // can't be used in orbit, or if holding player is in the ship
+            return __instance is not MapDevice || (!StartOfRound.Instance.mapScreen.overrideCameraForOtherUse && (__instance.playerHeldBy == null || (!__instance.playerHeldBy.isInHangarShipRoom && !__instance.playerHeldBy.isInElevator)));
+        }
+
+        static IEnumerator pingMapSystem(MapDevice mapDevice, PlayerControllerB playerHeldBy)
+        {
+            mapDevice.mapCamera.gameObject.SetActive(true);
+            if (!GameNetworkManager.Instance.localPlayerController.isInHangarShipRoom && !GameNetworkManager.Instance.localPlayerController.isInElevator)
+            {
+                mapDevice.mapAnimatorTransition.SetTrigger("Transition");
+                StartOfRound.Instance.mapScreenPlayerName.gameObject.SetActive(false);
+            }
+            yield return new WaitForSeconds(0.035f);
+            Vector3 playerPos = playerHeldBy.transform.position;
+            playerPos.y += 3.636f;
+            mapDevice.mapCamera.transform.position = playerPos;
+            yield return new WaitForSeconds(0.2f);
+            mapDevice.mapLight.enabled = playerHeldBy.isInsideFactory || playerHeldBy.transform.position.y < -80f;
+            mapDevice.mapCamera.Render();
+            mapDevice.mapLight.enabled = false;
+            mapDevice.mapCamera.gameObject.SetActive(false);
+            StartOfRound.Instance.mapScreenPlayerName.gameObject.SetActive(true);
+            yield break;
+        }
+
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.SwitchMapMonitorPurpose))]
+        [HarmonyPostfix]
+        static void PostSwitchMapMonitorPurpose(bool displayInfo)
+        {
+            // reset screens in orbit
+            if (displayInfo)
+            {
+                foreach (MapDevice mapDevice in Object.FindObjectsOfType<MapDevice>())
+                {
+                    if (!mapDevice.mapCamera.CompareTag("MapCamera"))
+                    {
+                        RenderTexture rt = RenderTexture.active;
+                        RenderTexture.active = mapDevice.mapCamera.targetTexture;
+                        GL.Clear(true, true, Color.clear);
+                        RenderTexture.active = rt;
+                    }
+                }
             }
         }
     }
